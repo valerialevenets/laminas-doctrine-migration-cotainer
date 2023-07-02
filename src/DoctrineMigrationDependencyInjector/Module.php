89c98ed7b1673298e87f4b44d5d4809091d1779f
Valeria Levenets <valeria.levenets94@gmail.com>
2023-07-02 16:29:56 +0300

    init

--- src/DoctrineMigrationDependencyInjector/Module.php
+++ src/DoctrineMigrationDependencyInjector/Module.php
@@ -6,6 +6,10 @@ use Laminas\Mvc\MvcEvent;
 
 class Module
 {
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
 
     public function onBootstrap(MvcEvent $event)
     {
